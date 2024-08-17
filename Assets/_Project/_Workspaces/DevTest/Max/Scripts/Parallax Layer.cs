d6c6fff844eb631203952b9c5e51ec1a6724368e
Max-Pieters <66798505+Max-Pieters@users.noreply.github.com>
2024-08-17 10:15:52 +0200

    Reorganize files, implement parallax effect
    Basic parallax effect now implemented. Change _effectStrength to set effect. See documentation for details.
    
    At the moment, the camera needs to be added manually to each layer with parallax effect
    
    In test scene, change player position to see effect in action.

---
+++ Assets/_Project/_Workspaces/DevTest/Max/Scripts/Parallax Layer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScript : MonoBehaviour
+{
+    private float _startPosition;
+    public GameObject _camera; 
+
+    /*
+        Set the strenght of the parallax effect.
+        Strength < 0 for foreground layer
+        Strength = 0 for middle layer
+        Strength > 0 for background layer
+        strength = 1 for non-moving background layer (ie the sun)
+    */
+    [SerializeField] private float _effectStrength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosition = _startPosition + _camera.transform.position.x * _effectStrength;
+        transform.position = new Vector3(newPosition, transform.position.y, transform.position.z);
+    }
+}
