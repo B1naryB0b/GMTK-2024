b96218e6eb74d884b5785ae9e19f95650500e73e
Max-Pieters <66798505+Max-Pieters@users.noreply.github.com>
2024-08-17 11:39:32 +0200

    Check if droplets are close

--- Assets/_Project/_Workspaces/DevTest/Max/Scripts/Droplet Controller.cs
+++ Assets/_Project/_Workspaces/DevTest/Max/Scripts/Droplet Controller.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class DropletController : MonoBehaviour
 {
+    [SerializeField] private float _attractionRadius;
+    //private ArrayList<GameObject> droplets;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(Transform droplet in transform) 
+        {
+            if (IsClose(droplet)) 
+            {
+                float xManDist = player.transform.position.x - droplet.position.x;
+                float xDist = xManDist * xManDist;
+                float yManDist = player.transform.position.y - droplet.position.y;
+                float yDist = yManDist * yManDist;
+                float dist = Mathf.Sqrt(xDist + yDist);
+
+                if (dist > _attractionRadius) 
+                {
+                    continue;
+                }
+
+                // Move to player
+            }
+        }
+    }
+
+    private bool IsClose(Transform droplet) {
+        return Mathf.Abs(player.transform.position.x - droplet.position.x) < _attractionRadius
+            && Mathf.Abs(player.transform.position.y - droplet.position.y) < _attractionRadius;
     }
 }
