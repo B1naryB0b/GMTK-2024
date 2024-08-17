e66e51e932c2e29a6cb2bc6d6fdaf27b20a91cb9
Max-Pieters <66798505+Max-Pieters@users.noreply.github.com>
2024-08-17 12:38:31 +0200

    Make droplets go towards the player

--- Assets/_Project/_Workspaces/DevTest/Max/Scripts/Droplet Controller.cs
+++ Assets/_Project/_Workspaces/DevTest/Max/Scripts/Droplet Controller.cs
@@ -5,39 +5,42 @@ using UnityEngine;
 public class DropletController : MonoBehaviour
 {
     [SerializeField] private float _attractionRadius;
-    //private ArrayList<GameObject> droplets;
+    [SerializeField] private float _attractionSpeed;
     public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach(Transform droplet in transform) 
+        foreach(Transform transform in transform) 
         {
-            if (IsClose(droplet)) 
+            if (IsClose(transform)) 
             {
-                float xManDist = player.transform.position.x - droplet.position.x;
+                float xManDist = player.transform.position.x - transform.position.x;
                 float xDist = xManDist * xManDist;
-                float yManDist = player.transform.position.y - droplet.position.y;
+                float yManDist = player.transform.position.y - transform.position.y;
                 float yDist = yManDist * yManDist;
                 float dist = Mathf.Sqrt(xDist + yDist);
-
                 if (dist > _attractionRadius) 
                 {
                     continue;
                 }
 
-                // Move to player
+    	        GameObject droplet = transform.gameObject;
+                Debug.Log(droplet);
+                Rigidbody2D rb = droplet.GetComponent<Rigidbody2D>();
+                rb.velocity = new Vector3(xManDist * _attractionSpeed, yManDist * _attractionSpeed, 0);
             }
         }
     }
 
-    private bool IsClose(Transform droplet) {
-        return Mathf.Abs(player.transform.position.x - droplet.position.x) < _attractionRadius
-            && Mathf.Abs(player.transform.position.y - droplet.position.y) < _attractionRadius;
+    private bool IsClose(Transform transform) {
+        return Mathf.Abs(player.transform.position.x - transform.position.x) < _attractionRadius
+            && Mathf.Abs(player.transform.position.y - transform.position.y) < _attractionRadius;
     }
 }
