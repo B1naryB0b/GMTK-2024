65f6b89f73927b257c1410ad692354f4809fb691
B1naryB0b <35399675+B1naryB0b@users.noreply.github.com>
2024-08-19 21:47:09 +0100

    Main Menu Edits

--- Assets/_Project/_Workspaces/DevTest/Belle/Scripts/MainMenu.cs
+++ Assets/_Project/_Workspaces/DevTest/Belle/Scripts/MainMenu.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private AudioManager _audioMan;
     // Start is called before the first frame update
-    public void Start()
-    {
-        _audioMan.PlaySound();
-    }
+    
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
